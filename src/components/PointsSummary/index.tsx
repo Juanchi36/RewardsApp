58477e53571d39a44c4d161402f9cd3c623a5ac1
Juan Martin Gonzalez <juan.gonzalez@longevo.com>
2023-04-18 22:50:47 -0300

    feat: points summary

---
+++ src/components/PointsSummary/index.tsx
@@ -0,0 +1,52 @@
+import React, { FunctionComponent } from 'react';
+import { formatPoints } from '@helpers';
+import styled from 'styled-components/native';
+
+/**
+ * Types
+ */
+
+interface PointsSummaryProps {
+  ammount: number;
+  month: string;
+}
+
+/**
+ * Styled components
+ */
+
+const SummaryWrapper = styled.View`
+  background-color: #334ffa;
+  height: 143px;
+  border-radius: 20px;
+  shadow-opacity: 0.5;
+  shadow-radius: 20px;
+  shadow-offset: 0px 4px;
+  elevation: 1;
+  justify-content: center;
+`;
+
+const MonthText = styled.Text`
+  color: #ffffff;
+  font-weight: 800;
+  font-size: 16px;
+  line-height: 21.86px;
+  position: absolute;
+  top: 21px;
+  left: 18px;
+`;
+
+const AmountText = styled.Text`
+  color: #ffffff;
+  font-weight: 800;
+  font-size: 32px;
+  line-height: 43.71px;
+  text-align: center;
+`;
+
+export const PointsSummary: FunctionComponent<PointsSummaryProps> = ({ ammount, month }) => (
+  <SummaryWrapper testID="points-summaty-container">
+    <MonthText>{month}</MonthText>
+    <AmountText>{`${formatPoints(ammount)} pts`}</AmountText>
+  </SummaryWrapper>
+);
